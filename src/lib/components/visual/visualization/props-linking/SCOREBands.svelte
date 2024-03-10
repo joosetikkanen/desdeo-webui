e427ad96f02102dd2e74083708a2b3fa68e2f236
Joose Tikkanen <jopitikk@student.jyu.fi>
2024-03-10 17:51:04 +0200

    add axis repositioning functionality for SCORE bands component

--- src/lib/components/visual/visualization/props-linking/SCOREBands.svelte
+++ src/lib/components/visual/visualization/props-linking/SCOREBands.svelte
@@ -1,6 +1,6 @@
-<!-- PlotlyChart.svelte -->
+<!-- SCOREBands.svelte -->
 
-<script>
+<script lang="ts">
   import { onMount } from "svelte";
 
   // Seems that plotly.js only works in the browser, so have to use dynamic import
@@ -8,8 +8,8 @@
     const Plotly = await import("plotly.js-dist-min");
 
     var trace1 = {
-      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-      y: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+      x: [0, 0.5, 1, 1, 0.5, 0],
+      y: [0, 0.4, 0.9, 1, 0.5, 0.1],
       fill: "tozerox",
       fillcolor: "rgba(0,100,80,0.2)",
       line: { color: "transparent", shape: "spline" },
@@ -19,10 +19,8 @@
     };
 
     var trace2 = {
-      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-      y: [
-        5.5, 3, 5.5, 8, 6, 3, 8, 5, 6, 5.5, 4.75, 5, 4, 7, 2, 4, 7, 4.4, 2, 4.5,
-      ],
+      x: [0, 0.5, 1, 1, 0.5, 0],
+      y: [1, 0.5, 0.1, 0, 0.4, 0.9],
       fill: "tozerox",
       fillcolor: "rgba(0,176,246,0.2)",
       line: { color: "transparent", shape: "spline" },
@@ -32,8 +30,8 @@
     };
 
     var trace3 = {
-      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-      y: [11, 9, 7, 5, 3, 1, 3, 5, 3, 1, -1, 1, 3, 1, -0.5, 1, 3, 5, 7, 9],
+      x: [0, 0.5, 1, 1, 0.5, 0],
+      y: [0, 0.9, 0, 0.1, 1, 0.1],
       fill: "tozerox",
       fillcolor: "rgba(231,107,243,0.2)",
       line: { color: "transparent", shape: "spline" },
@@ -43,15 +41,18 @@
     };
 
     var trace4 = {
-      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      line: { color: "rgb(0,100,80)", shape: "spline" },
+      x: [0, 0.5, 1, 1, 0.5, 0],
+      y: [0.9, 0, 0.9, 1, 0.1, 1],
+      fill: "tozerox",
+      fillcolor: "rgba(255, 20, 20, 0.2)",
+      line: { color: "transparent", shape: "spline" },
       mode: "lines",
       name: "Fair",
+      showlegend: false,
       type: "scatter",
     };
 
-    var trace5 = {
+    /* var trace5 = {
       x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       y: [5, 2.5, 5, 7.5, 5, 2.5, 7.5, 4.5, 5.5, 5],
       line: { color: "rgb(0,176,246)", shape: "spline" },
@@ -67,25 +68,27 @@
       mode: "lines",
       name: "Ideal",
       type: "scatter",
-    };
+    }; */
 
-    var data = [trace1, trace2, trace3, trace4, trace5, trace6];
+    var data = [trace1, trace2, trace3, trace4];
 
     var layout = {
       paper_bgcolor: "rgb(255,255,255)",
       plot_bgcolor: "rgb(229,229,229)",
       xaxis: {
         gridcolor: "rgb(255,255,255)",
-        range: [1, 10],
+        range: [0, 1],
         showgrid: true,
         showline: false,
         showticklabels: true,
         tickcolor: "rgb(127,127,127)",
         ticks: "outside",
         zeroline: false,
+        tickvals: [0, 0.5, 1],
       },
       yaxis: {
         gridcolor: "rgb(255,255,255)",
+        range: [0, 1],
         showgrid: false,
         showline: false,
         showticklabels: false,
@@ -96,12 +99,109 @@
       dragmode: false,
     };
     Plotly.newPlot("plotlyChart", data, layout);
+
+    var slider = document.getElementById("slider");
+    var sliderValue = document.getElementById("sliderValue");
+    var dropdown = document.getElementById("dropdown");
+
+    slider.addEventListener("input", function () {
+      sliderValue.textContent = this.value;
+
+      var [newData, newLayout] = getUpdatedChartData(
+        this.value,
+        dropdown.value
+      );
+
+      Plotly.react("plotlyChart", newData, newLayout);
+    });
+
+    dropdown.addEventListener("change", function () {
+      var selectedObjectiveIndex = dropdown.value;
+      var updatedSliderValue = trace1.x[selectedObjectiveIndex] * 100;
+
+      slider.value = updatedSliderValue;
+      sliderValue.textContent = updatedSliderValue + "";
+    });
+
+    function getUpdatedChartData(sliderValue, dropdownValue) {
+      // For now, let's just use the existing trace1 as an example
+      let updatedTraces = [];
+      for (let trace of data) {
+        let updatedTrace = { ...trace };
+
+        updatedTrace.x[dropdownValue] = sliderValue / 100;
+        updatedTrace.x[updatedTrace.x.length - 1 - dropdownValue] =
+          sliderValue / 100;
+
+        updatedTraces.push(updatedTrace);
+      }
+
+      /*
+      let newX = [...trace1.x]
+      newX[dropdownValue] = sliderValue / 100
+      newX[newX.length - 1 - dropdownValue] = sliderValue / 100
+
+      var updatedTrace = {
+        x: newX,
+        y: [0, 0.4, 0.9, 1, 0.5, 0.1],
+        fill: "tozerox",
+        fillcolor: "rgba(0,100,80,0.2)",
+        line: { color: "transparent", shape: "spline" },
+        name: "Fair",
+        showlegend: false,
+        type: "scatter",
+      }; */
+
+      let newTickVals = [...layout.xaxis.tickvals];
+      newTickVals[dropdownValue] = sliderValue / 100;
+
+      var updatedLayout = {
+        paper_bgcolor: "rgb(255,255,255)",
+        plot_bgcolor: "rgb(229,229,229)",
+        xaxis: {
+          gridcolor: "rgb(255,255,255)",
+          range: [0, 1],
+          showgrid: true,
+          showline: false,
+          showticklabels: true,
+          tickcolor: "rgb(127,127,127)",
+          ticks: "outside",
+          zeroline: false,
+          tickvals: newTickVals,
+        },
+        yaxis: {
+          gridcolor: "rgb(255,255,255)",
+          range: [0, 1],
+          showgrid: false,
+          showline: false,
+          showticklabels: false,
+          tickcolor: "rgb(127,127,127)",
+          ticks: "outside",
+          zeroline: false,
+        },
+        dragmode: false,
+      };
+
+      //trace1 = updatedTrace;
+      data = updatedTraces;
+      layout = updatedLayout;
+      return [updatedTraces, updatedLayout];
+    }
   });
 </script>
 
-<!-- Your Plotly chart goes here -->
-<div id="plotlyChart" />
+<div>
+  <div>
+    <select id="dropdown">
+      <option value="0">Objective 1</option>
+      <option value="1">Objective 2</option>
+      <option value="2">Objective 3</option>
+    </select>
+    <input type="range" id="slider" min="0" max="100" value="0" />
+    <span id="sliderValue">0</span>
+  </div>
+  <div id="plotlyChart" />
+</div>
 
 <style>
-  /* Your component styles go here */
 </style>
